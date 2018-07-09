47341fc1da50570db283d12033282dd8f795e212
Karen Mac <karenbtlai@gmail.com>
2018-07-09 16:10:44 -0700

    comment out unfinished file

--- src/__test__/lib/item-mock.js
+++ src/__test__/lib/item-mock.js
@@ -1,35 +1,35 @@
-import faker from 'faker';
-import createAccountMockPromise from './accountMock';
+// import faker from 'faker';
+// // import createAccountMockPromise from './accountMock';
 
-const createItemMockPromise = () => {
-  const mockData = {};
+// const createItemMockPromise = () => {
+//   const mockData = {};
 
-  return createItemMockPromise()
-    .then((mockItemData) => {
-      mockData.account = mockItemData.account;
-      mockData.token = mockItemData.token;
+//   return createItemMockPromise()
+//     .then((mockItemData) => {
+//       mockData.account = mockItemData.account;
+//       mockData.token = mockItemData.token;
       
-      const mockItem = {
-        postType: faker.lorem.word(),
-        itemType: faker.lorem.words(2),
-        accountId: mockItemData.account._id,
-      };
-      return new Item(mockItem).save();
-    })
-    .then((item) => {
-      mockData.item = item;
-      return mockData;
-    })
-    .catch((err) => {
-      throw err;
-    });
-};
+//       const mockItem = {
+//         postType: faker.lorem.word(),
+//         itemType: faker.lorem.words(2),
+//         accountId: mockItemData.account._id,
+//       };
+//       return new Item(mockItem).save();
+//     })
+//     .then((item) => {
+//       mockData.item = item;
+//       return mockData;
+//     })
+//     .catch((err) => {
+//       throw err;
+//     });
+// };
 
-const removeAllResources = () => {
-  return Promise.all([
-    Item.remove({}),
-    removeAccountMockPromise(),
-  ]);
-};
+// const removeAllResources = () => {
+//   return Promise.all([
+//     Item.remove({}),
+//     removeAccountMockPromise(),
+//   ]);
+// };
 
-export { createItemMockPromise, removeAllResources };
+// export { createItemMockPromise, removeAllResources };
