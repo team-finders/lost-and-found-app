98d4017b6ee41918c1a609fe349cd3bfe1de6fac
Liz Petersen <lizpetersen928@gmail.com>
2018-07-10 17:57:11 -0700

    updates items-router with routes to aws urls

--- src/__test__/items-router.test.js
+++ src/__test__/items-router.test.js
@@ -32,11 +32,13 @@ describe('ITEM ROUTER', () => {
         const returnItem = await superagent.post(`${apiUrl}/items`)
           .auth(testAccount.account.username, testAccount.originalRequest.password)
           .set('Authorization', `Bearer ${testAccount.token}`)
-          .send(mockItem);
+          .field(mockItem)
+          .attach('image', `${__dirname}/assets/orange-water-bottle.jpg`);
         expect(returnItem.status).toEqual(200);
         expect(returnItem.body.postType).toEqual('Lost');
         expect(returnItem.body.itemType).toEqual('water bottle');
         expect(returnItem.body._id).toBeTruthy();
+        expect(returnItem.body.image.url).toBeTruthy();
       } catch (err) {
         expect(err).toEqual('something bad');
       }
--- src/router/items-router.js
+++ src/router/items-router.js
@@ -1,20 +1,37 @@
+import multer from 'multer';
 import { Router } from 'express';
 import HttpErrors from 'http-errors';
 import Item from '../model/items';
 import bearerAuthMiddleware from '../lib/middleware/bearer-auth-middleware';
+import { s3Upload, s3Remove } from '../lib/s3';
 import logger from '../lib/logger';
 import permit from '../lib/middleware/permissions-middleware';
 
+const multerUpload = multer({ dest: `${__dirname}/../temp` });
+
 const itemsRouter = new Router();
 
-itemsRouter.post('/api/items', bearerAuthMiddleware, permit('account'), (request, response, next) => {
+itemsRouter.post('/api/items', bearerAuthMiddleware, permit('account'), multerUpload.any(), (request, response, next) => {
   if (!request.account) return next(new HttpErrors(400, 'POST REQUEST to ITEM ROUTER: Invalid Request'));
+  if (!request.files || request.files.length !== 1) {
+    return next(new HttpErrors(400, 'IMAGE ROUTER POST REQUEST: invalid request'));
+  } 
+  
+  const [file] = request.files;
+  logger.log(logger.INFO, `ITEMS ROUTER POST TO AWS: valid file ready to upload: ${JSON.stringify(file, null, 2)}`);
+ 
+  const key = `${file.fileName}.${file.originalname}`;
 
-  Item.init()
-    .then(() => {
+  return s3Upload(file.path, key)
+    .then((url) => {
+      logger.log(logger.INFO, `IMAGE ROUTER POST: received a valid url from Amazon S3: ${url}`);
       return new Item({
         ...request.body,
         accountId: request.account._id,
+        image: {
+          url,
+          fileName: key,
+        },
       }).save();
     })
     .then((item) => {
@@ -22,7 +39,6 @@ itemsRouter.post('/api/items', bearerAuthMiddleware, permit('account'), (request
       return response.json(item);
     })
     .catch(next);
-  return undefined;
 });
 
 itemsRouter.get('/api/items/:id?', bearerAuthMiddleware, (request, response, next) => {
@@ -60,4 +76,29 @@ Item.findOne({ 'postType': 'Found' }, { _id: request.params.id }, callback(err, 
 
 */
 
+itemsRouter.delete('/api/items/:id?', bearerAuthMiddleware, permit('account'), (request, response, next) => {
+  if (!request.account) return next(new HttpErrors(401, 'ITEMS ROUTER DELETE: invalid request'));
+  if (!request.params.key) return next(new HttpErrors(400, 'ITEMS ROUTER DELETE: no id provided'));  
+  return Item.findById(request.params.id)
+    .then((image) => {
+      if (!image) return next(new HttpErrors(404, 'IMAGE ROUTE DELETE: no image found'));
+      const key = image.fileName;
+      return s3Remove(key);
+    })
+    .then((result) => {
+      return response.json(result);
+      // logger.log(logger.INFO, 'IMAGE ROUTER DELETE: successfully deleted image');
+    })
+    .catch(next);
+});
+
 export default itemsRouter;
+
+
+/*
+  needs:
+  aws upload
+  capture req.img from .attach('img', path/to/img/)
+  attach returned url from aws.upload to req.body
+  save item with all props attached 
+  */
