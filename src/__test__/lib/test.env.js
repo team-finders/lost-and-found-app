3f8d32fbecdd73719759b22b10343755a1a008e2
Liz Petersen <lizpetersen928@gmail.com>
2018-07-11 17:07:48 -0700

    fixes test env

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -3,7 +3,7 @@ process.env.PORT = 5000;
 process.env.MONGODB_URI = 'mongodb://localhost/testing-foundit';
 process.env.SECRET_KEY = 'randomstdringasdfasf309484950495';
 
-const isAwsMock = false;
+const isAwsMock = true;
 
 if (isAwsMock) {
   process.env.AWS_BUCKET = 'fake';
